9171bd8019c7b549e80687f7b519fb7b3de80868
PC-UNI <pc.uni@gmail.com>
2025-12-02 10:05:06 -0500

    ADD pc

--- Punteros/APC3/mogollon-sebastian-pc4-p2.cpp
+++ Punteros/APC3/mogollon-sebastian-pc4-p2.cpp
@@ -1,3 +1,74 @@
-//
-// Created by SEBASTIAN on 2/12/2025.
-//
+/*
+Se tiene (ya existe ) el archivo de texto llamado ENCUESTA.txt, el cual contiene por cada linea de voto o no de un encuestado por uno o mas
+candidatos a las elecciones presidenciales. Cada candidato esta representado por un numero de 3 cifras y luego una S que indica que el
+encuestado si votaria por ese candidato y N representa que no votaria por ese candidato. A continuacion una parte del archivo.
+Archivo.txt:
+120 S
+140 S
+110 N
+140 N
+110 S
+130 S
+140 N
+120 S
+120 N
+110 S
+Se observa:
+120 2 (S) y 1 (N)
+140 1 (S) y 2 (N)
+110 2 (S) y 1 (N)
+130 1 (S) y 0 (N)
+*/
+#include <iostream>
+#include <fstream>
+#include <cmath>
+using namespace std;
+
+struct nodo {
+    int cod;
+    char voto;
+    nodo *punt;
+};
+
+nodo *piv;
+
+void creaNodo(nodo *&p, int cod, char voto) {
+        if (p==NULL) {
+            p=new nodo;
+            p->cod=cod;
+            p->voto=voto;
+            p->punt=NULL;
+        }else {
+            creaNodo(p->punt, cod, voto);
+        }
+    }
+
+
+void creaLista(nodo *&p) {
+    int cod;
+    char voto;
+    ifstream ent;
+    ent.open ( "PROMEDIOS.txt");
+    if (!ent) {
+        cout<<"Error al abrir el archivo."<<endl;
+    } else {
+        while (ent>>cod>>voto) {
+            creaNodo(p, cod, voto);
+        }
+        ent.close();
+    }
+}
+
+void recorre (nodo *&p) {
+    if (p!=NULL) {
+        cout<<p->cod<<"\t"<<p->voto;
+        recorre(p->punt);
+    }
+}
+
+int main () {
+    piv=NULL;
+    creaLista(piv);
+    recorre(piv);
+    return 0;
+}
--- Punteros/APC3/mogollon-sebastian-pc4-p3.cpp
+++ Punteros/APC3/mogollon-sebastian-pc4-p3.cpp
@@ -1,3 +1,122 @@
-//
-// Created by SEBASTIAN on 2/12/2025.
-//
+/*
+Desarrolle un programa que lea un archivo de texto "Vuelos txt
+codigo(ente de 4 dig) destino(Hasta 25 caracte) asientos tot (ent) asientos disp( ent) precio base (real))
+Asuma que los Datos estan separados por 1 o mas espacios en blanco
+1. Cargar los registros del archhivo en una lista enlazada. PAra cada vuelo ademas calcule el porcentaje de ocupacion
+2. Ordene la lista de mayor a menor porcentaje de ocupacion y si  hay empate por precio base descendente. Muestre la lista ordenada
+*/
+#include <iostream>
+#include <fstream>
+#include <cmath>
+using namespace std;
+
+struct nodo {
+    int cod;
+    char dest;
+    int asienT;
+    int asienD;
+    float precio;
+    float porc;
+    nodo *punt;
+};
+
+nodo *piv;
+
+void creaNodo(nodo *p, int cod, char dest, char asienT, char asienD, float precio) {
+    if (p==NULL) {
+        float porc;
+        porc=(asienT-asienD)/(asienT)*100;
+        p=new nodo;
+        p->cod = cod;
+        p->dest = dest;
+        p->asienT = asienT;
+        p->asienD = asienD;
+        p->precio = precio;
+        p->porc = porc;
+        p->punt = NULL;
+    }else {
+        creaNodo(p->punt, cod, dest, asienT, asienD, precio);
+    }
+}
+
+void creaLista(nodo *&p) {
+    int cod;
+    char dest;
+    int asienT;
+    int asienD;
+    float precio;
+    ifstream ent;
+    ent.open ( "VUELOS.txt");
+    if (!ent) {
+        cout<<"Error al abrir el archivo."<<endl;
+    } else {
+        while (ent>>cod>>dest>>asienT>>asienD>>precio) {
+            creaNodo(p, cod, dest, asienT, asienD, precio);
+        }
+        ent.close();
+    }
+}
+
+
+void intercambia (nodo *&q1, nodo *&q2) {
+    int auxCod=q1->cod;
+    q1->cod=q2->cod;
+    q2->cod=auxCod;
+
+    char auxDest=q1->dest;
+    q1->dest=q2->dest;
+    q2->dest=auxDest;
+
+    int auxAsienT=q1->asienT;
+    q1->asienT=q2->asienT;
+    q2->asienT=auxAsienT;
+
+    int auxAsienD=q1->asienD;
+    q1->asienD=q2->asienD;
+    q2->asienD=auxAsienD;
+
+    float auxPrecio=q1->precio;
+    q1->precio=q2->precio;
+    q2->precio=auxPrecio;
+
+    float auxPorc=q1->porc;
+    q1->porc=q2->porc;
+    q2->porc=auxPorc;
+}
+
+void ordena(nodo *&p) {
+    nodo *q1, *q2;
+    int auxC;
+    float auxP;
+    q1 = p;
+    while(q1!=NULL){
+        q2 = q1->punt;
+        while(q2!=NULL){
+            if(q1->porc < q2->porc){
+                intercambia (q1, q2);
+            }
+            if (q1->porc==q2->porc) {
+                if (q1->precio < q2->precio) {
+                    intercambia (q1, q2);
+                }
+            }
+            q2 = q2->punt;
+        }
+        q1=q1->punt;
+    }
+}
+
+void recorre (nodo *&p) {
+    if (p!=NULL) {
+        cout<<p->cod<<"\t"<<p->dest<<"\t"<<p->asienT<<"\t"<<p->asienD<<"\t"<<p->precio<<endl;
+        recorre(p->punt);
+    }
+}
+
+int main () {
+    piv=NULL;
+    creaLista(piv);
+    ordena(piv);
+    recorre(piv);
+    return 0;
+}
