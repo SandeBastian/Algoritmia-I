b9584bde319470ad44979583e582fe667e7b2ef5
El_Regente <El_Regenta@gmail.com>
2025-12-02 03:46:30 -0500

    ADD

--- Punteros/APC3/2025.1.pc4.p2.cpp
+++ Punteros/APC3/2025.1.pc4.p2.cpp
@@ -1,3 +1,16 @@
-//
-// Created by SEBASTIAN on 2/12/2025.
-//
+/*Se tiene un archivo NOTAS.txt (ya existe) que contiene letras mayusculas
+(correspondiente a secciones) y valores numericos entre 0 y 20. A continuacion una
+parte del archivo
+NOTAS.txt:
+c12 A15 C8 B11 A10
+D11 A11 B6 B10 C11
+---
+De donde se puede observar que solo para dicha parte del archivo:
+Las notas de la sección C son: 12, 8 y 11
+Las notas de la seccion A son: 15, 10 y 11
+LAs notas de la seccion B son: 11 y 6
+LA nota de la seccion D es: 11
+Diseñe un programa que empleando el archivo Notas.txt y la programacion dinamica (listas
+enlazadas) nos muestre la seccion o seccions con el mayor nro de aprobados y el promedio
+de notas de dicha seccon o secciones
+*/
