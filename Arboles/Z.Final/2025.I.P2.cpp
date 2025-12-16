626295e2b0bf8f69bdeda1b092c5ae23d65cca56
El Regente <elregente@gmail.com>
2025-12-16 05:19:00 -0500

    ADD

--- Arboles/Z.Final/2025.I.P2.cpp
+++ Arboles/Z.Final/2025.I.P2.cpp
@@ -9,31 +9,62 @@ si no existen elementos con esta cualidad mostrar 0.
 b) Hallar y mostrar la suma de los cuadrados de los elementos impares de la interna.
 Ejemplo: Si n=7.
 */
+/*
+Presente un programa en C++, que, genere aleatoriamente una matriz cuadrada de orden n>=5 (n
+debe ser impar) de números enteros de 2 cifras.
+*/
 #include <iostream>
-#include <fstream>
-#include <cstring>
+#include <cmath>
+#include <cstdlib>
+#include <ctime>
 using namespace std;
+
 int main () {
-    //srand (time(NULL));
+    srand(time(NULL));
+
     int n;
     do {
         cout<<"Ingrese el orden de la matriz (n>=5 y n impar): ";
         cin>>n;
-        if (n<5||n%2==0) cout<<"Orden invalido"<<endl;
-    }while (n<5||n%2==0);
+        if (n<5 || n%2==0) cout<<"Orden invalido"<<endl;
+    } while (n<5 || n%2==0);
 
     int mat[n][n];
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < n; j++) {
+
+    for (int i=0;i<n;i++)
+        for (int j=0;j<n;j++)
             mat[i][j] = 10 + rand()%90;
+
+    cout<<"\nMatriz:\n";
+    for (int i=0;i<n;i++){
+        for (int j=0;j<n;j++)
+            cout<<mat[i][j]<<"\t";
+        cout<<endl;
+    }
+
+    int c = n/2;
+    int sumaExt = 0;
+    int sumaInt = 0;
+
+    for (int i=0;i<n;i++){
+        for (int j=0;j<n;j++){
+            int d = abs(i-c) + abs(j-c);
+
+            if (d > c) {                 // EXTERIOR (sin rombo)
+                int r = sqrt(mat[i][j]);
+                if (r*r == mat[i][j])
+                    sumaExt += r;
+            }
+            else if (d < c) {            // INTERIOR (sin rombo)
+                if (mat[i][j] % 2 != 0)
+                    sumaInt += mat[i][j] * mat[i][j];
+            }
+            // d == c  → rombo → NO SE USA
         }
     }
 
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < n; j++) {
-            cout<<mat[i][j]<<"\t";
-        }
-        cout<<endl;
-    }
+    cout<<"\nSuma de elementos externos con raiz cuadrada exacta = "<<sumaExt<<endl;
+    cout<<"Suma de los cuadrados de los elementos internos impares = "<<sumaInt<<endl;
+
     return 0;
 }
